2117e9b554b2efd1b6f9ca43eac1e826ca2bf381
Qveys <quentinveys@gmail.com>
2025-03-29 17:32:41 +0100

    App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Time Tracker</h1>
+            <p>Welcome to the Time Tracker App!</p>
+        </div>
+    );
+};
+
+export default App;
