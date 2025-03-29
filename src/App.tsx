a22071c209c35f3dcaed39b3754dcca9bded49bf
Qveys <quentinveys@gmail.com>
2025-03-29 17:48:47 +0100

    SettingsPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import DashboardPage from "./pages/DashboardPage.tsx";
 import HistoryPage from "./pages/HistoryPage.tsx";
 import ProjectsPage from "./pages/ProjectsPage.tsx";
+import SettingsPage from "./pages/SettingsPage.tsx";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/history" element={<HistoryPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Router>
   );
 };
