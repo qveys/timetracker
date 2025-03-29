238d839a3f5c8ab6bfc5a1362c7c3a88d55d8ed7
Qveys <quentinveys@gmail.com>
2025-03-29 17:48:42 +0100

    ProjectsPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import DashboardPage from "./pages/DashboardPage.tsx";
 import HistoryPage from "./pages/HistoryPage.tsx";
+import ProjectsPage from "./pages/ProjectsPage.tsx";
 
 const App: React.FC = () => {
   return (
       <Router>
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/history" element={<HistoryPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
       </Router>
   );
 };
