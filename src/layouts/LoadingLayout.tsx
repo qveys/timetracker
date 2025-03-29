87a66ed48f76d4f882d67b47c7cc701a84662dfd
Qveys <quentinveys@gmail.com>
2025-03-29 17:55:45 +0100

    LoadingLayout.tsx

---
+++ src/layouts/LoadingLayout.tsx
@@ -0,0 +1,7 @@
+import { Clock } from "lucide-react";
+
+export const LoadingLayout = () => (
+    <div className="loading-screen">
+      <Clock className="w-12 h-12 text-blue-600 dark:text-blue-400 animate-spin" />
+    </div>
+);
